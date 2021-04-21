50fcedc0c9b203797da3d08475307731cebec648
San1357 <san1357@gmail.com>
2021-04-21 13:23:07 +0530

    sloved flake8 err of api of multiple booking

--- multiple_booking_api.py
+++ multiple_booking_api.py
@@ -1,10 +1,6 @@
-from flask import Flask, jsonify, request
-import json
+from flask import Flask, request
 import pprint
-
 from multiple_booking_ticket import MultipleBooking
-
-
 app = Flask(__name__)
 
 
@@ -13,22 +9,19 @@ def hello():
     print("Welcome")
 
 
-
 @app.route('/multiple_bookings', methods=['POST'])
 def multiple_bookings():
     if request.method == "POST":
         req_json = request.json
-        pprint.pprint(req_json) # pprint will print your response in response json format.
+        pprint.pprint(req_json)  # pprint will print your response in response json format.
 
         ticket_booking = MultipleBooking()
         ticket_booking.parsing(req_json)
         ticket_booking.book_seat()
 
         response = ticket_booking.create_response()
-        
         return response
 
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
