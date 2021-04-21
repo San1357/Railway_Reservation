e312a4aea25183956412ac16c6e4452de506110a
San1357 <san1357@gmail.com>
2021-04-21 12:46:35 +0530

    added hello endpoint to check the endpoint is working or not

---
+++ multiple_booking_api.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+import json
+import pprint
+
+from multiple_booking_ticket import MultipleBooking
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    print("Welcome")
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
