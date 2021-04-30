b8e0bba8505d26376500f6cd5c3e5cc1890a0de0
San1357 <san1357@gmail.com>
2021-04-30 13:25:45 +0530

    initialising the functionality as pass for update traindetail in database

--- database.py
+++ database.py
@@ -26,3 +26,20 @@ class Database:
 
     def delete_row_from_passenger_details(self, pnr_no):
         pass
+
+    def update_traindetail(self, avail_seat, train_no):
+        pass
+
+
+if __name__ == "__main__":
+    db = Database()
+    db.get_DB_Train_Details()
+    db.get_DB_avail_seats()
+    db.update_DB_train_detail()
+    db.update_passengerdetail()
+    db.all_from_passenger_info()
+    db.all_from_train_Details()
+    db.all_from_passenger_info()
+    db.all_from_train_Details()
+    db.delete_row_from_passenger_details()
+    db.update_traindetail()
