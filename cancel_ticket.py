5ad06124057ba43f92d84bc50c19584e920eb7c3
San1357 <san1357@gmail.com>
2021-04-30 12:39:55 +0530

    calling all the function from database.py which are needed

--- cancel_ticket.py
+++ cancel_ticket.py
@@ -8,33 +8,27 @@ class CancelTicket():
 
     def get_cancel_ticket(self):
         print("Pnr_no:", self.pnr_no)
-        
         db = Database()
         self.rows = db.all_from_passenger_info(self.pnr_no)
         print(self.rows)
         print(type(self.rows))
         print("train no:", self.rows[7])
-        
-        cur.execute(
-            "select * from traindetail where train_no = %s", ([self.rows[7]])
-        )
-        self.rows2 = list(cur.fetchone())
+
+        self.train_no = self.rows[7]
+        self.rows2 = db.all_from_train_Details(self.train_no)
+
         print(self.rows2)
         print(type(self.rows2))
         print("avail_seat", self.rows2[5])
+        self.avail_seat = self.rows2[5]
 
-        cur.execute(
-            "delete from passenger_details where pnr_number in (%s)", ([
-                self.pnr_no]))
-        cur.execute(
-            "update traindetail set avail_seat = %s + %s where train_no = %s", ([
-                self.rows2[5], incrementby1, self.rows[7]
-            ]))
-        conn.commit()
+        db.delete_row_from_passenger_details(self.pnr_no)
+        db.update_traindetail(self.avail_seat, self.train_no)
         print("Your Ticket is  Cancelled!!!")
         print("ThankYou !! Visit Again!! ")
         return self.pnr_no
 
 
-ticket_cancel = CancelTicket(1618944043898834)
-ticket_cancel.get_cancel_ticket()
+if __name__ == "__main__":
+    ticket_cancel = CancelTicket(1619445211108680)
+    ticket_cancel.get_cancel_ticket()
