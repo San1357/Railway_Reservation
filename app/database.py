f7bae0e40b49f6c19fd249bb0e4a38e60d69b54c
San1357 <san1357@gmail.com>
2021-05-21 17:57:20 +0530

    some basic modification in database file

--- app/database.py
+++ app/database.py
@@ -38,10 +38,10 @@ class Database:
 
     def get_DB_Train_Details(self, fromstation, tostation):
         self.cur.execute(
-            "select * from traindetail where fromstation = %s and tostation = %s",
+            "select * from Train_details where from_station = %s and to_station = %s",
             (fromstation, tostation))
         e = list(self.cur.fetchone())
-        print(e)
+        # print(e)
         return e
     
     def get_trainId_from_traindetails(self, train_no):
@@ -55,17 +55,7 @@ class Database:
         op = (self.cur.fetchone())
         print("op:", op)
         return op
-
     
-    def get_DB_avail_seats(self, train_no):
-        self.cur.execute("select * from traindetail where train_no = %s", ([
-            train_no]))
-        f = list(self.cur.fetchone())
-        self.avail_seat = f[5]
-        self.train_no = f[2]
-        print("train_no:", self.train_no)
-        print("f:", f)
-        return f
 
     def set_passenger_uid_for_user_details(self):
         self.cur.execute("SELECT uuid_generate_v4(); ")
@@ -74,17 +64,27 @@ class Database:
         print("gt:", gt)
         return gt
 
+    def get_DB_avail_seats(self, train_no):
+        self.cur.execute("select * from Train_details where train_no = %s", ([
+            train_no]))
+        f = list(self.cur.fetchone())
+        self.avail_seat = f[5]
+        self.train_no = f[3]
+        print("train_no:", self.train_no)
+        print("f:", f)
+        return f
+
     def update_DB_train_detail(self, avail_seat, train_no):
-        self.cur.execute("update traindetail set avail_seat = %s where train_no = %s", (
+        self.cur.execute("update Train_details set avail_seats = %s where train_no = %s", (
             avail_seat, train_no))
         g = self.conn.commit()
         print("g:", g)
         return g
 
-    def update_passengerdetail(self, passenger_detail_list_of_tuple):
-        passenger_detail_list_of_tuple
-        self.cur.executemany("insert into passenger_details(name, email, age, aadhaar_no, fromstation, tostation, class, train_no, pnr_number)values(%s, %s,%s, %s,%s, %s, %s, %s, %s)", passenger_detail_list_of_tuple)
+    def update_passengerdetail(self, passenger_detail_list_of_tuple ):
+        self.cur.executemany("insert into user_details(passenger_uid, name, age, email, aadhaar_no)values(%s, %s,%s, %s, %s)", passenger_detail_list_of_tuple)
         h = self.conn.commit()
+        print("h:", h)
         return h
 
     def update_pnr_details(self, pnr_list_value):
@@ -133,7 +133,7 @@ class Database:
 
     def all_from_passenger_info(self, pnr_no):
         self.cur.execute(
-            "select * from passenger_details where pnr_number = %s", ([pnr_no]))
+            "select * from user_details where pnr_number = %s", ([pnr_no]))
         a = list(self.cur.fetchone())
         print("a:", a)
         return a
@@ -144,23 +144,22 @@ class Database:
         print("eu:", eu)
         return eu
 
-     def all_from_booking_details(self, pnr_id):
+    def all_from_booking_details(self, pnr_id):
         self.cur.execute("select * from booking_details where pnr_id = %s", ([pnr_id]))
         pu = list(self.cur.fetchone())
         print("pu:", pu)
         return pu
+        
 
-    def all_from_train_Details(self, train_no):
-        self.cur.execute(
-            "select * from traindetail where train_no = %s", ([train_no])
-        )
+    def all_from_train_Details(self, t_id):
+        self.cur.execute("select * from train_details where t_id = %s", ([t_id]))
         b = list(self.cur.fetchone())
         print("b:", b)
         return b
 
     def delete_row_from_passenger_details(self, pnr_no):
         self.cur.execute(
-            "delete from passenger_details where pnr_number in (%s)", ([
+            "delete from user_details where pnr_number in (%s)", ([
                 pnr_no]))
         c = self.conn.commit()
         return c
@@ -172,12 +171,18 @@ class Database:
         cu = self.conn.commit()
         return cu
 
+    def delete_row_from_pnr_details(self, pnr_id):
+        self.cur.execute(
+            "delete from pnr_details where pnr_id in (%s)", ([
+                pnr_id]))
+        cut = self.conn.commit()
+        return cut
 
-    def update_traindetail(self, avail_seat, train_no):
+    def update_traindetail(self, avail_seat, train_id):
         incrementby1 = 1
         self.cur.execute(
-            "update traindetail set avail_seat = %s + %s where train_no = %s", ([
-                avail_seat, incrementby1, train_no
+            "update train_details set avail_seats = %s + %s where t_id = %s", ([
+                avail_seat, incrementby1, train_id
             ]))
         o = self.conn.commit()
         return o
@@ -195,19 +200,21 @@ class Database:
 if __name__ == "__main__":
 
     pnr_list_value = pnr_generator(1)
-    passenger_detail_list_of_tuple = [('jeet', 'jeet105@gmail.com', 19, 1491041, 'Noida', 'Gorakhpur', 'General', 12110,)]
+    passenger_detail_list_of_tuple = [('jeet', 19, 'jeet105@gmail.com', 1491041, 'f4322e3c-a304-4177-b8c4-56b45b1bdd28', 1619692464584002,)]
     print(passenger_detail_list_of_tuple)
-    print(pnr_list_value)
+    '''print(pnr_list_value)
     passenger_detail_list_of_tuple[0] = passenger_detail_list_of_tuple[0] + pnr_list_value[0]
     db = Database()
-    db.get_DB_Train_Details("Gorakhpur", "Pune")
+    # db.get_DB_Train_Details("Gorakhpur", "Pune")
+    db.get_trainId_from_traindetails(12110)
     db.get_DB_avail_seats(12110)
-    db.update_DB_train_detail(22, 12110)
+    db.update_DB_train_detail(22, 12110)'''
+    db = Database()    
     db.update_passengerdetail(passenger_detail_list_of_tuple)
-    db.all_from_passenger_info(1619692464584002)
+    '''db.all_from_passenger_info(1619692464584002)
     db.all_from_train_Details(12110)
     db.all_from_passenger_info(1619692464584002)
     db.all_from_train_Details(12110)
     db.delete_row_from_passenger_details(1619692464584002)
     db.update_traindetail(22, 12110)
-    db.pnr_of_pnr_status(7934082101)
+    db.pnr_of_pnr_status(7934082101)'''
