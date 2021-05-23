a5825cbe3a278690ca58bdbbfa8184712d1aeef1
San1357 <san1357@gmail.com>
2021-05-23 15:06:57 +0530

    some more basic changes

--- app/cancel_ticket.py
+++ app/cancel_ticket.py
@@ -3,21 +3,20 @@ from database import Database
 
 class CancelTicket():
 
-    def __init__(self, pnr_no):
-        self.pnr_no = pnr_no
+    def __init__(self):
+        self.db = Database()
 
-    def get_cancel_ticket(self):
-        db = Database()
-        pnr_details = db.all_from_pnr_details(self.pnr_no)
+    def get_cancel_ticket(self, pnr_no):
+        pnr_details = self.db.all_from_pnr_details(pnr_no)
         pnr_id = pnr_details[0]
-        booking_details = db.all_from_booking_details(pnr_id)
+        booking_details = self.db.all_from_booking_details(pnr_id)
         t_id = booking_details[1]
-        train_details = db.all_from_train_details(t_id)
-        db.delete_row_from_booking_details(pnr_id)
-        db.delete_row_from_pnr_details(pnr_id)
+        train_details = self.db.all_from_train_details(t_id)
+        self.db.delete_row_from_booking_details(pnr_id)
+        self.db.delete_row_from_pnr_details(pnr_id)
         avail_seat = train_details[5]
-        db.update_seats_in_traindetail(avail_seat, t_id)
-        print("Pnr_no:", self.pnr_no)
+        self.db.update_seats_in_traindetail(avail_seat, t_id)
+        print("Pnr_no:", pnr_no)
         print("pnr_details:", pnr_details)
         print("train_details:", train_details)
         print("booking_details:", booking_details)
@@ -28,5 +27,5 @@ class CancelTicket():
 
 
 if __name__ == "__main__":
-    ticket_cancel = CancelTicket(1621750529727682)
-    ticket_cancel.get_cancel_ticket()
+    ticket_cancel = CancelTicket()
+    ticket_cancel.get_cancel_ticket(1621750529727682)
