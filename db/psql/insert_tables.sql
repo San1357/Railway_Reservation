fdaf76201b98b42217033107b581744450c82457
San1357 <san1357@gmail.com>
2021-05-25 11:01:49 +0530

    inserting in pnr_details database

--- db/psql/insert_tables.sql
+++ db/psql/insert_tables.sql
@@ -14,3 +14,10 @@ insert into user_details (passenger_uid, name, age, email, aadhaar_no) values (u
 insert into train_details (t_id, from_station, to_station, train_no, total_seats, avail_seats, train_schedule)
 values (uuid_generate_v4(), 'Pune', 'Gorakhpur', 12345, 200, 19, '{GORAKHPUR_JN,BASTI,GONDA,BARABANKI,LUCKNOW,KANPUR_CENTRAL,JHANSI,BHOPAL,ITARSI,BHUSAVAL,MANMAD,AHMADNAGAR,PUNE_JN}'),
 (uuid_generate_v4, 'Mumbai', 'Gorakhpur', 500129, 180, 59, '{LOKMANYATILAK,BHUSAVAL,ITARSI,BHOPAL,JHANSI,ORAI,POKHRAYAN,KANPUR_CENTRAL,AISHBAGH,BADSHAHNAGAR,GONDA,BASTI,KHALILABAD,GORAKHPUR}');
+
+
+--insert into pnr_details table
+
+
+insert into pnr_details (pnr_id, pnr_number) values (uuid_generate_v4(), 1612344455854958);
+
