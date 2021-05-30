5c05dc279675ddbdec3acf07a3d2fca818ba34a6
San1357 <san1357@gmail.com>
2021-05-30 17:17:50 +0530

    modification in funcn search_api cause of modification in train_search.py file

--- app/main.py
+++ app/main.py
@@ -36,9 +36,10 @@ def search_api():
         date = req_json['date']
         print(date)
 
-        train_search_object = Search(from_station, to_station, date)
-        train_search_object.train_search()
+        train_search_object = Search()
+        train_search_object.train_search(from_station, to_station, date)
         detail_of_train = train_search_object.get_train_details()
+        response_of_train = train_search_object.train_details_response()
         schedule_of_train = train_search_object.train_schedule()
         print(detail_of_train)
         return jsonify({"Your Details are": detail_of_train, "Train Schedule are": schedule_of_train})
