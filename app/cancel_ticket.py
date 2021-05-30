d58b7dea7bb26ccdbaea623454b0d1d53e165b16
San1357 <san1357@gmail.com>
2021-05-30 19:17:49 +0530

    added another function enter_pnr_no

--- app/cancel_ticket.py
+++ app/cancel_ticket.py
@@ -6,26 +6,44 @@ class CancelTicket():
     def __init__(self):
         self.db = Database()
 
-    def get_cancel_ticket(self, pnr_no):
-        pnr_details = self.db.all_from_pnr_details(pnr_no)
-        pnr_id = pnr_details[0]
-        booking_details = self.db.all_from_booking_details(pnr_id)
-        t_id = booking_details[1]
-        train_details = self.db.all_from_train_details(t_id)
+    def enter_pnr_no(self, pnr_no):
+        self.pnr_no = pnr_no
+        print("pnr_no", self.pnr_no)
+        self.pnr_details = self.db.all_from_pnr_details([self.pnr_no])
+        return self.pnr_no
+
+    def get_cancel_ticket(self):
+        print("pnr:", self.pnr_no)
+        # self.pnr_details = self.db.all_from_pnr_details(self.pnr_no)
+        pnr_uuid = self.pnr_details[0]
+        booking_details = self.db.all_from_booking_details(pnr_uuid)
+        train_uuid = booking_details[1]
+        train_details = self.db.all_from_train_details(train_uuid)
         avail_seat = train_details[5]
-        self.db.delete_row_from_booking_details(pnr_id)
-        self.db.delete_row_from_pnr_details(pnr_id)
-        self.db.update_seats_in_train_details(avail_seat, t_id)
-        print("Pnr_no:", pnr_no)
-        print("pnr_details:", pnr_details)
+        self.db.delete_row_from_booking_details(pnr_uuid)
+        self.db.delete_row_from_pnr_details(pnr_uuid)
+        self.db.update_seats_in_train_details(avail_seat, train_uuid)
+        print("Pnr_no:", self.pnr_no)
+        print("pnr_details:", self.pnr_details)
         print("train_details:", train_details)
         print("booking_details:", booking_details)
-        print("type_of_pnr_details:", type(pnr_details), "type_of_booking_details:", type(booking_details), "type_of_train_details:", type(train_details))
-        print("pnr_id:", pnr_details[0], "train_id:", booking_details[1], "avail_seat", train_details[5])
+        print("type_of_pnr_details:", type(self.pnr_details), "type_of_booking_details:", type(booking_details), "type_of_train_details:", type(train_details))
+        print("pnr_id:", self.pnr_details[0], "train_id:", booking_details[1], "avail_seat", train_details[5])
         print("Your Ticket is  Cancelled!!!")
         print("ThankYou !! Visit Again!! ")
+        
+    
+    
+    
+
+
+
+
 
 
 if __name__ == "__main__":
     ticket_cancel = CancelTicket()
-    ticket_cancel.get_cancel_ticket(16219351358472184)
+    ticket_cancel.enter_pnr_no(1622362531927666)
+    ticket_cancel.check_pnr_exist_or_not()
+    ticket_cancel.response_of_pnr()
+    
