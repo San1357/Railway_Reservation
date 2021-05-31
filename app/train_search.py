e5d1bfda6b7b5e870bcdc718be7adf13dca20e61
San1357 <san1357@gmail.com>
2021-05-31 15:13:58 +0530

    at line 26 conditional statement have been applied

--- app/train_search.py
+++ app/train_search.py
@@ -23,60 +23,70 @@ class Search():
         print("search_list:", search_list)
 
         self.train_details_info = self.db.get_all_from_train_details(self.from_station, self.to_station)
-
-        print("----------------- train_details_info ------------------")
-        print(self.train_details_info)
-        print(type(self.train_details_info))
-        print(type(self.train_details_info[0]))
-        print(type(self.train_details_info[1]))
-        print(type(self.train_details_info[2]))
-        print(type(self.train_details_info[3]))
-        print(type(self.train_details_info[4]))
-        print(type(self.train_details_info[5]))
-        print("train_uuid", self.train_details_info[0])
-        print("from station:", self.train_details_info[1])
-        print("to station:", self.train_details_info[2])
-        print("train no: ", self.train_details_info[3])
-        print("total no of seat:", self.train_details_info[4])
-        print("available seat: ", self.train_details_info[5])
+        if self.train_details_info is None:
+            print(None)
+        else:
+            print("----------------- train_details_info ------------------")
+            print(self.train_details_info)
+            print(type(self.train_details_info))
+            print(type(self.train_details_info[0]))
+            print(type(self.train_details_info[1]))
+            print(type(self.train_details_info[2]))
+            print(type(self.train_details_info[3]))
+            print(type(self.train_details_info[4]))
+            print(type(self.train_details_info[5]))
+            print("train_uuid", self.train_details_info[0])
+            print("from station:", self.train_details_info[1])
+            print("to station:", self.train_details_info[2])
+            print("train no: ", self.train_details_info[3])
+            print("total no of seat:", self.train_details_info[4])
+            print("available seat: ", self.train_details_info[5])
 
     def train_details_response(self):
-
+        
+        message = "sorry either the info is not available in our database or either you typed wrong"
         if self.train_details_info is not None:
-            if self.train_details_info[1] == self.from_station and self.train_details_info[2] == self.to_station:
-                print("** Here is your Train detail **")
-                print("Train_Id:", self.train_details_info[0])
-                print("Train NO:", self.train_details_info[3])
-                print("Total Seats are:", self.train_details_info[4])
-                print("Available Seats are:", self.train_details_info[5])
-                result = {
-                    "Train_Id": self.train_details_info[0],
-                    "Train_No": self.train_details_info[3],
-                    "Total Seats are": self.train_details_info[4],
-                    "Available Seats are": self.train_details_info[5]}
-                return result
-            else:
-                print(
-                    "sorry either the info is not available in our database or either you typed wrong")
+            print("** Here is your Train detail **")
+            print("Train_Id:", self.train_details_info[0])
+            print("Train NO:", self.train_details_info[3])
+            print("Total Seats are:", self.train_details_info[4])
+            print("Available Seats are:", self.train_details_info[5])
+            result = {
+                "Train_Id": self.train_details_info[0],
+                "Train_No": self.train_details_info[3],
+                "Total Seats are": self.train_details_info[4],
+                "Available Seats are": self.train_details_info[5]}
+        else:
+            result = {
+                "Train_Id": "did not found",
+                "Train_No": "didnt exist",
+                "status" : message
+            }
+            print(result)
+        return result
 
     def train_schedule(self):
 
         if self.train_details_info is not None:
-            if self.train_details_info[1] == self.from_station and self.train_details_info[2] == self.to_station:
-                print("** Train Schedule **")
-                results = self.train_details_info[6]
-                print(results)
-                print(type(results))
-                for i in results:
-                    print(i)
-                return results
+            print("** Train Schedule **")
+            results = self.train_details_info[6]
+            print(results)
+            print(type(results))
+            for i in results:
+                print(i)
+        else:
+            results = {
+                "status" : "The train detail you entered is not exist in our database."
+            }
+            print(results)
+        return results
 
 
 if __name__ == "__main__":
     print("---------------Class Init started---------------")
     search_train_object = Search()
     print("---------------train_Search()--------------")
-    search_train_object.train_search("gkp", "pune", "12-03-2021")
+    search_train_object.train_search("g", "pune", "12-03-2021")
     print("---------------get_train_details()--------------")
     search_train_object.get_train_details()
     print("--------------response of train details -----------")
