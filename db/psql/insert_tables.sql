e7793e763a1be6b7dd8f7794c0766ada9e562c05
San1357 <san1357@gmail.com>
2021-06-01 19:36:12 +0530

    insert demo uuid value after generating uuid from pg admin

--- db/psql/insert_tables.sql
+++ db/psql/insert_tables.sql
@@ -1,26 +1,16 @@
---insert into pnr_store table
+--insert user_details table 
 
-insert into pnr_store (pnr_no, status, pnrpresentstatus) values (7934082101, 'confirmed', 'yes'),(9274018223, 'confirmed','yes'), (9274072413, 'Not confirmed', 'No'), (7932632202, 'Not confirmed', 'No');
-
-
----insert user_details table 
-
-insert into user_details (passenger_uid, name, age, email, aadhaar_no) values (uuid_generate_v4(), 'jeet', 19, 'jeet105@gmail.com', 1491041);
+insert into user_details (passenger_uid, name, age, email, aadhaar_no) values ('88c85148-40fe-4367-889a-d73ca60c89ab', 'jeet', 19, 'jeet105@gmail.com', 1491041);
 
 --insert into train_details table 
-
-
 insert into train_details (t_id, from_station, to_station, train_no, total_seats, avail_seats, train_schedule)
-values (uuid_generate_v4(), 'Pune', 'Gorakhpur', 12345, 200, 19, '{GORAKHPUR_JN,BASTI,GONDA,BARABANKI,LUCKNOW,KANPUR_CENTRAL,JHANSI,BHOPAL,ITARSI,BHUSAVAL,MANMAD,AHMADNAGAR,PUNE_JN}'),
-(uuid_generate_v4, 'Mumbai', 'Gorakhpur', 500129, 180, 59, '{LOKMANYATILAK,BHUSAVAL,ITARSI,BHOPAL,JHANSI,ORAI,POKHRAYAN,KANPUR_CENTRAL,AISHBAGH,BADSHAHNAGAR,GONDA,BASTI,KHALILABAD,GORAKHPUR}');
+values ('4d19ae9d-0944-4eed-8072-2b0c084e300c', 'Pune', 'Gorakhpur', 12345, 200, 19, '{GORAKHPUR_JN,BASTI,GONDA,BARABANKI,LUCKNOW,KANPUR_CENTRAL,JHANSI,BHOPAL,ITARSI,BHUSAVAL,MANMAD,AHMADNAGAR,PUNE_JN}'),
+('09aa103f-bbda-434e-96dd-2de98623b82f', 'Mumbai', 'Gorakhpur', 500129, 180, 59, '{LOKMANYATILAK,BHUSAVAL,ITARSI,BHOPAL,JHANSI,ORAI,POKHRAYAN,KANPUR_CENTRAL,AISHBAGH,BADSHAHNAGAR,GONDA,BASTI,KHALILABAD,GORAKHPUR}');
 
 
 --insert into pnr_details table
+insert into pnr_details (pnr_id, pnr_number) values ('32dcd511-56bc-418f-b7a3-fa337eca2062', 1612344455854958);
 
+--insert into booking_details table
 
-insert into pnr_details (pnr_id, pnr_number) values (uuid_generate_v4(), 1612344455854958);
-
-
---insert into booking_details table 
-
-insert into booking_details (passenger_uid, t_id, pnr_id) values (uuid_generate_v4(), uuid_generate_v4, uuid_generate_v4());
+insert into booking_details (passenger_uid, t_id, pnr_id) values ('88c85148-40fe-4367-889a-d73ca60c89ab', '4d19ae9d-0944-4eed-8072-2b0c084e300c', '32dcd511-56bc-418f-b7a3-fa337eca2062');
