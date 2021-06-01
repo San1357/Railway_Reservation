5d526d88470c11192a67519e9de12034d404f0bc
San1357 <san1357@gmail.com>
2021-06-01 19:34:58 +0530

    solved flake8 err

--- db/psql/create_tables.sql
+++ db/psql/create_tables.sql
@@ -1,15 +1,14 @@
----it will create extension so that you can use uuid_generate_v4()
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create extension if not exists "uuid-ossp"
 
 -- Creation of user_details table
 create table if not exists user_details(
-    passenger_uid uuid NOT NULL primary key, 
+    passenger_uid uuid NOT NULL primary key,
     name varchar(30) NOT NULL,
     age  int NOT NULL,
     email varchar(40) NOT NULL UNIQUE,
     aadhaar_no int UNIQUE NOT NULL
-);
+)
 
 -- Creation of train_detail table
 create table if not exists train_details(
@@ -20,17 +19,17 @@ create table if not exists train_details(
     total_seats int NOT NULL,
     avail_seats int NOT NULL,
     train_schedule TEXT
-);
+)
 
 --Creation of pnr_details table
 create table if not exists pnr_details(
     pnr_id uuid NOT NULL primary key,
     pnr_number bigint NOT NULL UNIQUE
-);
+)
 
---Creation of booking_details table 
+--Creation of booking_details table
 create table if not exists booking_details(
     passenger_uid uuid REFERENCES user_details(passenger_uid),
     t_id uuid REFERENCES train_details(t_id),
     pnr_id uuid REFERENCES pnr_details(pnr_id)
-);
+)
